f558832284d0a5458f3226726951fcb3c0534235
I Eat Babies <christiankohnen50@gmail.com>
2024-10-08 10:38:56 -0500

    baseline

--- LINQ_Kohnen_Christian/Program.cs
+++ LINQ_Kohnen_Christian/Program.cs
@@ -1,2 +1,12 @@
 ﻿using System;
-gghjgfhjgfhj
+
+namespace LINQ_Kohnen_Christian
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Poo");
+        }
+    }
+}
