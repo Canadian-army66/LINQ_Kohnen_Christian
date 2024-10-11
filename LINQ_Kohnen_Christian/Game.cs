337541cf6ef6f457cc2a45c671dc1365996d3772
I Eat Babies <christiankohnen50@gmail.com>
2024-10-11 10:37:18 -0500

    huh?

---
+++ LINQ_Kohnen_Christian/Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Kohnen_Christian
+{
+    internal class Game
+    {
+        public string Title { get; }
+        public char Esrb { get; }
+        public string Genre { get; }
+
+        public Game(string titleParam, char esrbParam, string genreParam)
+        {
+            Title = titleParam;
+            Esrb = esrbParam;
+            Genre = genreParam;
+        }
+    }
+}
--- LINQ_Kohnen_Christian/Program.cs
+++ LINQ_Kohnen_Christian/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace LINQ_Kohnen_Christian
 {
@@ -6,7 +7,37 @@ namespace LINQ_Kohnen_Christian
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Poo");
+            Game[] games = new Game[]
+            {
+                new Game("Minecraft", 'E', "Action-Adventure"),
+                new Game("Titanfall", 'M', "Movement FPS"),
+                new Game("Lies of P", 'M', "Action-Adventure"),
+                new Game("Space Marine 2", 'M', "Thrisp Person Shooter"),
+                new Game("I Love You, Colonel Sanders!", 'E', "Dating Sim")
+            };
+            var shortGames = from game in games
+                             where game.Title.Length < 9
+                             select $"Game Title: {game.Title.ToUpper()}";
+
+            foreach(var game in shortGames)
+            {
+                Console.WriteLine(game);
+            }
+
+            var mineCraft = games.Where(game => game.Title == "Minecraft")
+                .Select(game => $"Title: {game.Title}, ESRB: {game.Esrb}, Genre: {game.Genre}");
+
+            Console.WriteLine(mineCraft.FirstOrDefault());
+
+            var eRated = from game in games
+                         where game.Esrb == 'E'
+                         select game.Esrb;
+
+            Console.WriteLine("E rated games:");
+            foreach(var game in eRated)
+            {
+                Console.Write(game);
+            }       
         }
     }
 }
